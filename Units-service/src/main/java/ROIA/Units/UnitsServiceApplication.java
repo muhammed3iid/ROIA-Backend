31c6dda6ee90635f3f8de25fb29bac9f25a8af20
muhammed3iid <muhammed3iid@gmail.com>
2024-06-21 02:35:05 +0300

    units service initial commit

---
+++ Units-service/src/main/java/ROIA/Units/UnitsServiceApplication.java
@@ -0,0 +1,13 @@
+package ROIA.Units;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitsServiceApplication.class, args);
+	}
+
+}
---
+++ Units-service/src/test/java/ROIA/Units/UnitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ROIA.Units;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
