aa084df21d73f94db44cd85da4169c3c3e4d7e81
YoussefAboud <youssefaboud44@gmail.com>
2024-06-22 03:33:08 +0300

    discovery server

--- discovery-server/src/main/java/ROIA/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/ROIA/discovery_server/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package ROIA.discoveryserver;
+package ROIA.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServerApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
 }
